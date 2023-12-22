2002c125961ff229be29590130dd0061296cd41b
elijahm3497 <elijahm3497@outlook.com>
2023-12-22 21:19:52 +0000

    Ranged enemy shooting mechanics

--- ranged_enemy.gd
+++ ranged_enemy.gd
@@ -1,22 +1,51 @@
 extends CharacterBody3D
 
-@export var PlayerTarget: Node3D
 
+@onready var shootDelayTimer = $ShootDelayTimer
 var player
 var speed = 5
-
+var canFire
+var fireDelay = 2.0
+@export var firing_vfx: PackedScene
+@export var bullet: PackedScene
 
 func _ready():
 	player = $"../Player"
 
-
-
 func _physics_process(delta):
+	var distance = position.distance_to(player.position)
+
 	velocity = Vector3.ZERO
-	if player:
+	
+	if distance > 5:
 		velocity = position.direction_to(player.position) * speed
 		look_at(player.global_transform.origin, Vector3.UP)
+	else: 
+
+		canFire = true
+		look_at(player.global_transform.origin, Vector3.UP)
+	if canFire == true and shootDelayTimer.is_stopped():
+		_shoot()
+		shootDelayTimer.start(fireDelay)
+		canFire = false
+		
 	move_and_slide()
+
+func _shoot():
+
+	var firing_effect_instance : GPUParticles3D = firing_vfx.instantiate()
+	firing_effect_instance.global_transform = $Gun/GunBarrel.global_transform
+	firing_effect_instance.scale = Vector3(1, 1, 1)
+	firing_effect_instance.emitting = true
+	var new_bullet = bullet.instantiate()
+	new_bullet.global_transform = $Gun/GunBarrel.global_transform
+	new_bullet.scale = Vector3(1, 1, 1)
+	var scene_root = get_tree().get_root().get_children()[0] #fetches first node of the loaded scene tree 
+	scene_root.add_child(new_bullet)
+	scene_root.add_child(firing_effect_instance)
+
+
+
 
 
 
