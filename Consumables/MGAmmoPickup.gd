a11c7042b9b76341b79769be86ef07ba2ab2c85d
elijahm3497 <elijahm3497@outlook.com>
2024-06-11 20:27:51 +0100

    Auto reload bug fix

--- Consumables/MGAmmoPickup.gd
+++ Consumables/MGAmmoPickup.gd
@@ -1,15 +1,5 @@
 extends Node3D
 
-
-
-
-
-
-
-
-
-
-
 func _on_area_3d_area_entered(area):
 	if area.is_in_group("Players"):
 		queue_free()
--- Weapons/GunClass.gd
+++ Weapons/GunClass.gd
@@ -13,6 +13,7 @@ var currentshotgunammo
 var maxshotgunAmmo = 2
 var maxmgAmmo = 30
 var ammoTotal
+var reloading
 @export var firing_vfx: PackedScene
 @export var bullet: PackedScene
 @export var weapons: Array[PackedScene]
@@ -20,18 +21,24 @@ var ammoTotal
 func _ready():
 	current_mg_ammo = maxmgAmmo
 	currentshotgunammo = maxshotgunAmmo
+
 func _process(delta):
-	if Input.is_action_pressed("shoot"):
+	if Input.is_action_pressed("shoot") and canFire == true:
 		_fire()
+		print(canFire)
 
 func _fire():
 	pass
 
 func _reload():
-	if machinegunActive == true:
-		ammoCount = maxmgAmmo
-	if shotgunActive == true:
-		ammoCount = maxshotgunAmmo
+
+		if machinegunActive == true:
+			ammoCount = maxmgAmmo
+			canFire = true
+
+		if shotgunActive == true:
+			ammoCount = maxshotgunAmmo
+
 	
 
 func _interact():
